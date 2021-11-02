4a1580b5a7f2f18eb62a7c318bcba56c0f7db772
exectionik <exectionik9@gmail.com>
2021-11-02 10:42:40 +0100

    Websocket backandk

--- backend/src/main/java/com/BabbleVerse/Message/MessageController.java
+++ backend/src/main/java/com/BabbleVerse/Message/MessageController.java
@@ -1,12 +1,14 @@
 package com.BabbleVerse.Message;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class MessageController {
-    MessageService messageService;
+    /*MessageService messageService;
     public MessageController(MessageService messageService){
 
         this.messageService=messageService;
@@ -14,4 +16,18 @@ public class MessageController {
     public MessageController(){
 
     }
+*/
+    private final SimpMessagingTemplate template;
+
+    @Autowired
+    MessageController(SimpMessagingTemplate template){
+        this.template = template;
+    }
+
+    @MessageMapping("/send/message")
+    public void sendMessage(String message){
+        System.out.println(message);
+        this.template.convertAndSend("/message",  message);
+    }
 }
+
