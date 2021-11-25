ba7558bbfd4cb7c84b6b50b643d60eb3d61790cd
simonheglasik <simonheglasik@gmail.com>
2021-11-25 10:49:25 +0100

    service req

--- backend/src/main/java/com/babbleverse/request/RequestService.java
+++ backend/src/main/java/com/babbleverse/request/RequestService.java
@@ -1,12 +1,15 @@
 package com.babbleverse.request;
 
+import com.babbleverse.user.User;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public interface RequestService {
-     Request getRequestInfo();
-     Request createNewRequest();
-     void requestAccepted();
-     void requestRejected();
+     Optional<Request> getRequestInfo(Long id);
+     Request createNewRequest(User sender, User receiver,RequestType requestType);
+     void requestAccepted(Request request);
+     void requestRejected(Request request);
 
 }
--- backend/src/main/java/com/babbleverse/request/RequestServiceImpl.java
+++ backend/src/main/java/com/babbleverse/request/RequestServiceImpl.java
@@ -1,4 +1,41 @@
 package com.babbleverse.request;
 
-public class RequestServiceImpl {
+import com.babbleverse.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class RequestServiceImpl implements RequestService{
+
+    private RequestRepository requestRepository;
+
+    @Autowired
+    public RequestServiceImpl(RequestRepository requestRepository){
+        this.requestRepository = requestRepository;
+    }
+
+    @Override
+    public Optional<Request> getRequestInfo(Long id) {
+        return requestRepository.findById(id);
+    }
+
+    @Override
+    public Request createNewRequest(User sender, User receiver, RequestType requestType) {
+        return requestRepository.save(new Request(sender,receiver,requestType));
+    }
+
+    @Override
+    public void requestAccepted(Request request) {
+
+        switch (request.getRequestType()){
+            case groupInvite:
+
+            case friendRequest:
+        }
+    }
+
+    @Override
+    public void requestRejected(Request request) {
+
+    }
 }
